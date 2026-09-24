c4a304cfc5de089602f1b33e2b6f1bac3d325755
Ryan Layer <ryla3858@colorado.edu>
2026-09-24 19:22:51 +0000

    rem bad tests

--- orig/unit_testing/test_math_lib.py
+++ orig/unit_testing/test_math_lib.py
@@ -27,7 +27,7 @@ class TestMathLib(unittest.TestCase):
 #
 #    def test_div_by_underflowed_float_returns_none(self):
 #        self.assertIsNone(math_lib.div(10, 1e-400))
-
+"""
     def test_add_floats_exact_equal_fails(self):
         # intentionally fails, run this one live to show why == is risky with floats
         self.assertEqual(math_lib.add(0.1, 0.2), 0.3)
@@ -55,7 +55,7 @@ class TestMathLib(unittest.TestCase):
         random.seed(42)
         second = math_lib.rand_norm(0, 1)
         self.assertEqual(first, second)
-
+"""
 
 class TestFileAdd(unittest.TestCase):
     def setUp(self):
